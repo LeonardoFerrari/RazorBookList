b690add4eec2498eb61063218b4963b6a75d6635
LeonardoFerrari <l201164@dac.unicamp.br>
2021-03-16 19:03:24 -0300

    Adicionar arquivos de projeto.

---
+++ RazorBookList/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RazorBookList.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
