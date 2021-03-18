bed48985f57f9d7bd5734626dbc7336b88360d8f
LeonardoFerrari <l201164@dac.unicamp.br>
2021-03-18 13:53:05 -0300

    Botão de edição dos dados funcionando, junto com as restrições de entrada de dados

---
+++ RazorBookList/Pages/BookList/Editar.cshtml.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorBookList.Models;
+
+namespace RazorBookList.Pages.BookList
+{
+    public class EditarModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public EditarModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task OnGet(int Id)
+        {
+            Book = await _db.Book.FindAsync(Id);
+
+        }
+
+        // Post handler
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var BookFromDb = await _db.Book.FindAsync(Book.Id);
+                BookFromDb.Name = Book.Name;
+                BookFromDb.Author = Book.Author;
+                BookFromDb.ISBN = Book.ISBN;
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return RedirectToPage();
+            }
+        }
+
+    }
+}
